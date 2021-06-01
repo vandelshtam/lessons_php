d657b41aa6d6f35f26b22dd1f0a4efe244cbb679
vandelshtam <mvlju977@gmail.com>
2021-06-01 08:48:52 +0300

    My twelve commit

--- registr_task_1/info.php
+++ registr_task_1/info.php
@@ -1,18 +1,10 @@
 <?php 
-//session_start();
-//var_dump($_SESSION);
-/*
-    if(isset($_SESSION['message'])){
-        $status=$_SESSION['message']['status'];
-        $text=$_SESSION['message']['text'];
-    echo "<p class=\"$status\">$text</p>";
-    unset ($_SESSION['message']);
-    }*/
-    $pdo= new PDO("mysql:host=localhost:8889; dbname=lessons_php","root","root"); 
-    function display_flash_message()
-    { 
-        if(isset($_SESSION['message'])){  
+$pdo= new PDO("mysql:host=localhost:8889; dbname=lessons_php","root","root"); 
+function display_flash_message()
+{ 
+    if(isset($_SESSION['message']))
+    {  
         echo $_SESSION['message'];
         unset($_SESSION['message']);
-        }
     }
+}
