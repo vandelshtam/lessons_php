423dad9324325d26b3354b99ee271abe7aa11ee6
vandelshtam <mvlju977@gmail.com>
2021-06-13 10:58:26 +0300

    37 commit

--- registr_task_1/security.php
+++ registr_task_1/security.php
@@ -6,27 +6,23 @@ require 'init.php';
 if($_GET['users_id']!=false or $_GET['users_id']!=NULL){
     $_SESSION['profil_id']=$_GET['users_id'];  
 }
-
-$auth_email=$_SESSION['login'];
-
-        if(isset($_POST['email']) and isset($_POST['password']))
-        {
-            $email=$_POST['email'];
-            $user=get_user_by_email($email, $pdo);
+if(isset($_POST['email']) and isset($_POST['password']))
+{
+    $email=$_POST['email'];
+    $user=get_user_by_email($email, $pdo);
     
-            if($email==$auth_email or empty($user) )
-            {
+    if($email==$_SESSION['login'] or empty($user) )
+    {
                     
-                    edit_credentials($_SESSION['profil_id'],$email, $pdo);
-                        
-                    set_flash_message('success','Вы успешно изменили данные!');
-                    redirect_to('profile');die();
+        edit_credentials($_SESSION['profil_id'],$email, $pdo);   
+        set_flash_message('success','Вы успешно изменили данные!');
+        redirect_to('profile');die();
                     
-            }
-            else
-            {
-                set_flash_message('danger','Такой email (логин) занят, пожалуйста введите другой email!');
-                redirect_to('security');die();
-            }
-        }
+    }
+    else
+    {
+        set_flash_message('danger','Такой email (логин) занят, пожалуйста введите другой email!');
+        redirect_to('security');die();
+    }
+}
 require 'page_security.php';
