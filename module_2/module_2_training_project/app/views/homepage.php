a96f6b795e635ab99c62f7ae578301a48ac3ce7c
vandelshtam <mvlju977@gmail.com>
2021-08-13 08:45:17 +0300

    64 commits

--- module_2/module_2_training_project/app/views/homepage.php
+++ module_2/module_2_training_project/app/views/homepage.php
@@ -40,9 +40,21 @@ $auth = new \Delight\Auth\Auth($pdo);
                     <div id="c_1" class="card border shadow-0 mb-g shadow-sm-hover" data-filter-tags="oliver kopyov">
                         <div class="card-body border-faded border-top-0 border-left-0 border-right-0 rounded-top">
                             <div class="d-flex flex-row align-items-center">
+                                <?php if($user['status']==0):?>
                                 <span class="status status-success mr-3">
                                     <span class="rounded-circle profile-image d-block " style="background-image:url('<?=$user['avatar'];?>'); background-size: cover;"></span>
                                 </span>
+                                <?php endif;?>
+                                <?php if($user['status']==1):?>
+                                <span class="status status-warning mr-3">
+                                    <span class="rounded-circle profile-image d-block " style="background-image:url('<?=$user['avatar'];?>'); background-size: cover;"></span>
+                                </span>
+                                <?php endif;?>
+                                <?php if($user['status']==2):?>
+                                <span class="status status-danger mr-3">
+                                    <span class="rounded-circle profile-image d-block " style="background-image:url('<?=$user['avatar'];?>'); background-size: cover;"></span>
+                                </span>
+                                <?php endif;?>
                                 <div class="info-card-text flex-1">
                                     <a href="javascript:void(0);" class="fs-xl text-truncate text-truncate-lg text-info" data-toggle="dropdown" aria-expanded="false">
                                         <?=$user['username'];?>
