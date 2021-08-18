b402214d7b1e218a5e82ebd4118240d75474b56a
vandelshtam <84850328+vandelshtam@users.noreply.github.com>
2021-08-18 15:28:29 +0300

    Delete module_1_project_QueryBuilder directory

--- module_1_project_QueryBuilder/project_QueryBuilder/QueryBuilder/start.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = include '/Applications/MAMP/htdocs/php/lessons_php/module_1/database/config.php';
-include '/Applications/MAMP/htdocs/php/lessons_php/module_1/database/QueryBuilder.php';
-include '/Applications/MAMP/htdocs/php/lessons_php/module_1/database/Connection.php';
-
-return new QueryBuilder(Connection::make($config['database']));
--- module_1_project_QueryBuilder/project_QueryBuilder/controller/list_of_all_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-$db = include '/Applications/MAMP/htdocs/php/lessons_php/module_1_project_QueryBuilder/project_QueryBuilder/QueryBuilder/start.php';
-$posts = $db->getAll('posts');
-
-include '/Applications/MAMP/htdocs/php/lessons_php/module_1_project_QueryBuilder/project_QueryBuilder/index_view.php';
